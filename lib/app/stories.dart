857e7700e09c497c675344decc517c9d0bb2a347
Christopher Lang <65340138+chrisalxlng@users.noreply.github.com>
2023-09-03 13:18:26 +0200

    chore(ui): Remove `example` widget

--- lib/app/stories.dart
+++ lib/app/stories.dart
@@ -1,12 +1,6 @@
 import 'package:pawpath/app/counter/counter.story.dart';
 import 'package:pawpath/app/ui/buttons/icon_button/icon_button.story.dart';
 import 'package:pawpath/app/ui/buttons/text_button/text_button.story.dart';
-import 'package:pawpath/app/ui/example/example.story.dart';
 import 'package:storybook_flutter/storybook_flutter.dart';
 
-final List<Story> stories = [
-  iconButtonStory,
-  textButtonStory,
-  exampleStory,
-  counterStory
-];
+final List<Story> stories = [iconButtonStory, textButtonStory, counterStory];
