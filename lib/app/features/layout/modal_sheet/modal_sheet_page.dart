36a14f3110167a9ad83516aecd27f9f3e06eda0a
Christopher Lang <65340138+chrisalxlng@users.noreply.github.com>
2023-09-06 17:05:49 +0200

    refactor(layout): Remove unnecessary `level` property

--- lib/app/features/layout/modal_sheet/modal_sheet_page.dart
+++ lib/app/features/layout/modal_sheet/modal_sheet_page.dart
@@ -2,16 +2,12 @@ import "package:flutter/material.dart";
 import "package:modal_bottom_sheet/modal_bottom_sheet.dart";
 import "package:pawpath/app/themes.dart";
 
-const double heightFactorDifferencePerLevel = 0.03;
-
 class ModalBottomSheetPage<T> extends Page<T> {
   final Widget? child;
-  final int level;
 
   const ModalBottomSheetPage({
     super.key,
     this.child,
-    this.level = 1,
   });
 
   @override
